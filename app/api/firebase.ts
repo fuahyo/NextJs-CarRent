d00b269b97e523e9b1176c71dea057477c8da66d
fuahyo <fuah566@gmail.com>
2024-01-17 15:45:36 +0700

    test

--- app/api/firebase.ts
+++ app/api/firebase.ts
@@ -4,7 +4,8 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-   // apiKey: "AIzaSyCuKzmSMWLY0bYiqNwCK_odKCJ7S5_bPEU",
+  //  baru
+  // apiKey: "AIzaSyCuKzmSMWLY0bYiqNwCK_odKCJ7S5_bPEU",
   // authDomain: "com-pro-id.firebaseapp.com",
   // projectId: "com-pro-id",
   // storageBucket: "com-pro-id.appspot.com",
@@ -12,6 +13,7 @@ const firebaseConfig = {
   // appId: "1:1068163337727:web:082d48d99449e62dcf2bca",
   // measurementId: "G-KD9ZLDMJJT"
 
+  //lama
   apiKey: "AIzaSyADActymj1VzcsuBDl3kVBMz_Tbih6wawE",
   authDomain: "company-profile-c9b59.firebaseapp.com",
   projectId: "company-profile-c9b59",
