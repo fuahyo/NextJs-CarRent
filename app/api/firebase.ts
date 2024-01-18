99d666ea1fb0bb8cecf83e8f1e65db5f4cc3eefd
Galang Dwi Juniar <99375881+fuahyo@users.noreply.github.com>
2024-01-18 10:16:11 +0700

    Update firebase.ts

--- app/api/firebase.ts
+++ app/api/firebase.ts
@@ -5,22 +5,22 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   //  baru
-  // apiKey: "AIzaSyCuKzmSMWLY0bYiqNwCK_odKCJ7S5_bPEU",
-  // authDomain: "com-pro-id.firebaseapp.com",
-  // projectId: "com-pro-id",
-  // storageBucket: "com-pro-id.appspot.com",
-  // messagingSenderId: "1068163337727",
-  // appId: "1:1068163337727:web:082d48d99449e62dcf2bca",
-  // measurementId: "G-KD9ZLDMJJT"
+  apiKey: "AIzaSyCuKzmSMWLY0bYiqNwCK_odKCJ7S5_bPEU",
+  authDomain: "com-pro-id.firebaseapp.com",
+  projectId: "com-pro-id",
+  storageBucket: "com-pro-id.appspot.com",
+  messagingSenderId: "1068163337727",
+  appId: "1:1068163337727:web:082d48d99449e62dcf2bca",
+  measurementId: "G-KD9ZLDMJJT"
 
   //lama
-  apiKey: "AIzaSyADActymj1VzcsuBDl3kVBMz_Tbih6wawE",
-  authDomain: "company-profile-c9b59.firebaseapp.com",
-  projectId: "company-profile-c9b59",
-  storageBucket: "company-profile-c9b59.appspot.com",
-  messagingSenderId: "789947858979",
-  appId: "1:789947858979:web:46dde5e6f8da738c377196",
-  measurementId: "G-PSYDWL5PC5",
+  // apiKey: "AIzaSyADActymj1VzcsuBDl3kVBMz_Tbih6wawE",
+  // authDomain: "company-profile-c9b59.firebaseapp.com",
+  // projectId: "company-profile-c9b59",
+  // storageBucket: "company-profile-c9b59.appspot.com",
+  // messagingSenderId: "789947858979",
+  // appId: "1:789947858979:web:46dde5e6f8da738c377196",
+  // measurementId: "G-PSYDWL5PC5",
 };
 
 const app = initializeApp(firebaseConfig);
